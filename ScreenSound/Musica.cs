9052dbc6bcf2f058b094a817ca7495d083b2da9c
labsfiap <labsfiap@PA206MICRO16>
2025-03-21 17:54:51 -0300

    Add project files.

---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,28 @@
+﻿class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Arista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel is true)
+        {
+            Console.WriteLine("Música dísponivel no Plano.");
+        }
+        else
+        {
+            Console.WriteLine("Assine o Plano+ para liberar a música.");
+        }
+    }
+
+    public void ExibirNArtista()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+    }
+}
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,20 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "teste1";
+musica1.artista = "teste1";
+musica1.duracao = 333;
+musica1.disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "teste2";
+musica2.artista = "teste2";
+musica2.duracao = 444;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+Console.WriteLine("");
+musica2.ExibirFichaTecnica();
+
+Console.WriteLine("");
+musica1.ExibirNArtista();
+musica2.ExibirNArtista();
+
