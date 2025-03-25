0bb8939dd7064adee512b45ea7559ac4fe134ddb
Breno Silva <brenosilvats18@gmail.com>
2025-03-25 17:46:26 -0300

    Feat: Criando a Classe Album.

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,16 +1,30 @@
-﻿Musica musica1 = new Musica();
-musica1.Nome = "teste1";
-musica1.Artista = "teste1";
-musica1.Duracao = 333;
-musica1.Disponivel = true;
-Console.WriteLine(musica1.Disponivel);
+﻿Album albumDoMatue = new Album();
+albumDoMatue.Nome = "Máquina do Tempo";
+
+Musica musica1 = new Musica();
+musica1.Nome = "Lágrimas de Diamante";
+musica1.Duracao = 224;
 
 Musica musica2 = new Musica();
-musica2.Nome = "teste2";
-musica2.Artista = "teste2";
-musica2.Duracao = 444;
-musica2.Disponivel = false;
+musica2.Nome = "O Vento";
+musica2.Duracao = 240;
 
-musica1.ExibirFichaTecnica();
-Console.WriteLine("");
-musica2.ExibirFichaTecnica();
+Musica musica3 = new Musica();
+musica3.Nome = "Teu Amor";
+musica3.Duracao = 222;
+
+Musica musica4 = new Musica();
+musica4.Nome = "Meu Mundo";
+musica4.Duracao = 219;
+
+Musica musica5 = new Musica();
+musica5.Nome = "S.O.S";
+musica5.Duracao = 200;
+
+albumDoMatue.AdicionarMusica(musica1);
+albumDoMatue.AdicionarMusica(musica2);
+albumDoMatue.AdicionarMusica(musica3);
+albumDoMatue.AdicionarMusica(musica4);
+albumDoMatue.AdicionarMusica(musica5);
+
+albumDoMatue.ExibirMusicasDoAlbum();
